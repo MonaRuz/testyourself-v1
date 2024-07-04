8079cd4add6f0fc82567c5b17a8da3c215939c95
MonaRuz <mona.ruz@seznam.cz>
2024-07-04 21:14:55 +0200

    finished styling Login component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,21 +4,35 @@ import Logo from "../components/Logo"
 import { MdOutlineSchool } from "react-icons/md"
 
 export default function Home() {
-  const navigate=useNavigate()
+	const navigate = useNavigate()
 
 	return (
-		<div className='h-dvh bg-black'>
+		<div className='h-dvh bg-black px-3'>
 			<Logo />
-			<div className="text-purple-200 text-9xl flex justify-center my-5">
+			<div className='text-purple-200 text-9xl flex justify-center my-5'>
 				<MdOutlineSchool />
 			</div>
-      <div className="p-3">
-        <p className="text-blue-200 text-center">Practice your knowledge in any field!Whether you're a student or self-taught,<br/> write your own questions and answers and rate your performance.</p>
-      </div>
-      <div className="flex justify-center gap-4 pt-12">
-        <Button onClick={()=>navigate("/login")} style={{backgroundColor: "#88FFB6",width:"130px",height:"40px"}}>Login</Button>
-        <Button onClick={()=>navigate("/register")} style={{backgroundColor: "#FFF48B",width:"130px",height:"40px"}}>Register</Button>
-      </div>
+			<div>
+				<p className='text-blue-200 text-center'>
+					Practice your knowledge in any field!Whether you're a student or
+					self-taught,
+					<br /> write your own questions and answers and rate your performance.
+				</p>
+			</div>
+			<div className='flex justify-center gap-4 pt-12'>
+				<Button
+					onClick={() => navigate("/login")}
+					style={{ backgroundColor: "#88FFB6", width: "130px", height: "40px",fontFamily:"kanit" }}
+				>
+					Login
+				</Button>
+				<Button
+					onClick={() => navigate("/register")}
+					style={{ backgroundColor: "#FFF48B", width: "130px", height: "40px",fontFamily:"kanit" }}
+				>
+					Register
+				</Button>
+			</div>
 		</div>
 	)
 }
