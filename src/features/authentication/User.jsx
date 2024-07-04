7dd5620455c9fff7dd3975df53cd7b34cc1c4678
MonaRuz <mona.ruz@seznam.cz>
2024-07-04 19:53:44 +0200

    added image and text to home page

--- src/features/authentication/User.jsx
+++ src/features/authentication/User.jsx
@@ -1,6 +1,11 @@
-
+import { FaRegUserCircle } from "react-icons/fa"
 export default function User() {
-  return (
-    <div>User</div>
-  )
+	return (
+		<div className='text-green-200 flex gap-2'>
+			<div className='pt-1'>
+				<FaRegUserCircle />
+			</div>
+			<p>user</p>
+		</div>
+	)
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,16 @@
-import Logo from "../components/Logo";
+import Logo from "../components/Logo"
+import { MdOutlineSchool } from "react-icons/md"
 
 export default function Home() {
-	return <div className="h-dvh bg-black">
-    <Logo/>
-  </div>
+	return (
+		<div className='h-dvh bg-black'>
+			<Logo />
+			<div className="text-purple-200 text-9xl flex justify-center my-5">
+				<MdOutlineSchool />
+			</div>
+      <div className="p-2">
+        <p className="text-blue-200 text-center">Practice your knowledge in any field!Whether you're a student or self-taught,<br/> write your own questions and answers and rate your performance.</p>
+      </div>
+		</div>
+	)
 }
