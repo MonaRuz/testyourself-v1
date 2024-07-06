ef719e67fad0827e09240aba4269a40b074c0b4c
MonaRuz <mona.ruz@seznam.cz>
2024-07-06 15:26:29 +0200

    Styled CategoryStats component

--- src/features/categories/CategoryOverview.jsx
+++ src/features/categories/CategoryOverview.jsx
@@ -8,11 +8,11 @@ export default function CategoryOverview() {
 			<h2 className='text-purple-200 border-b border-purple-200 text-center pb-1 text-lg'>
 				Category overview
 			</h2>
-			<p className='text-blue-200 text-center text-sm py-3'>
+			<p className='text-blue-200 text-center text-sm py-3 sm:text-base'>
 				Start test, add new questions or search in existing questions to edit
 				them.
 			</p>
-			<div className="flex justify-center">
+			<div className='flex justify-center mb-3'>
 				<Button
 					style={{
 						backgroundColor: "#88FFB6",
@@ -24,8 +24,10 @@ export default function CategoryOverview() {
 					Add questions
 				</Button>
 			</div>
-			<CategoryStats />
-			<Questions />
+			<div className="flex flex-col items-center ">
+				<CategoryStats />
+				<Questions />
+			</div>
 		</div>
 	)
 }
--- src/features/categories/CategoryStats.jsx
+++ src/features/categories/CategoryStats.jsx
@@ -1,6 +1,33 @@
+import { MdDelete } from "react-icons/md"
 
 export default function CategoryStats() {
-  return (
-    <div>Category</div>
-  )
+	return (
+		<div className='border border-yellow-200 px-2 max-w-lg'>
+			<div className='flex justify-between items-center border-b border-purple-200'>
+				<h3 className='text-purple-200 py-2'>Category</h3>
+				<div className='text-red-200 text-xl'>
+					<MdDelete />
+				</div>
+			</div>
+				<table className='table-auto text-sm mt-2 text-left border-separate border-spacing-x-10 border-spacing-y-1 pb-2 m-auto'>
+					<tr>
+						<th className='text-blue-200 font-normal'>Questions:</th>
+						<th className='text-yellow-200 font-normal'>83</th>
+					</tr>
+					<tr>
+						<th className='text-blue-200 font-normal'>Progress:</th>
+						<th className='text-yellow-200 font-normal'>25 / 83</th>
+					</tr>
+					<tr>
+						<th className='text-blue-200 font-normal'>Current score:</th>
+						<th className='text-yellow-200 font-normal'>50 %</th>
+					</tr>
+					<tr>
+						<th className='text-blue-200 font-normal'>Highscore:</th>
+						<th className='text-yellow-200 font-normal'>80 %</th>
+					</tr>
+				</table>
+
+		</div>
+	)
 }
