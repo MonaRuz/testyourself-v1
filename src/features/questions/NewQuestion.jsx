1fc6cb4321a75d444d345413876e86fb0f32114d
MonaRuz <mona.ruz@seznam.cz>
2024-07-08 11:39:52 +0200

    Styled NewQuestion component

--- src/features/questions/NewQuestion.jsx
+++ src/features/questions/NewQuestion.jsx
@@ -1,24 +1,45 @@
+import { useNavigate } from "react-router-dom"
+import Button from "../../components/Button"
+
 export default function NewQuestion() {
-  function handleNewQuestion(){
-    //new question handling
-  }
+
+	function handleNewQuestion() {
+		//new question handling
+	}
 
 	return (
 		<div>
-			<p className="text-blue-200 text-center p-3">
-				Create new question for category CATEGORY. All fields must be
-				fulfilled.
+			<p className='text-blue-200 text-center p-3 text-sm sm:text-base mt-3'>
+				Create new question for category CATEGORY. All fields must be fulfilled.
 			</p>
-      <div>
-        <form onSubmit={handleNewQuestion} >
-          <label >
-            <textarea className="bg-black"></textarea>
-          </label>
-          <label>
-            <textarea className="bg-black"></textarea>
-          </label>
-        </form>
-      </div>
+			<div className="mt-3">
+				<form
+					onSubmit={handleNewQuestion}
+					className='flex flex-col items-center'
+				>
+					<label className='text-blue-200 text-center'>
+						{" "}
+						New question:<br></br>
+						<textarea className='bg-black border border-yellow-200 mt-2 w-72'></textarea>
+					</label>
+					<label className='text-blue-200 text-center'>
+						{" "}
+						New answer:<br></br>
+						<textarea className='bg-black border border-yellow-200 mt-2 w-72'></textarea>
+					</label>
+					<div className='flex justify-center mt-5'>
+						<Button
+							onClick={handleNewQuestion}
+							style={{
+								backgroundColor: "#88FFB6",
+								width: "130px",
+								height: "40px",
+								fontFamily: "kanit",
+							}}
+						>Add question</Button>
+					</div>
+				</form>
+			</div>
 		</div>
 	)
 }
