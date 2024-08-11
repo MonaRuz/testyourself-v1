ab1f46c7b5ec1953e08d4574d3964a793b8a4ae7
MonaRuz <mona.ruz@seznam.cz>
2024-08-11 21:41:44 +0200

    created function for fetching all categories

--- src/services/apiCategories.js
+++ src/services/apiCategories.js
@@ -3,9 +3,11 @@ import db from "../firebase/config"
 
 export async function getCategories() {
 	const querySnapshot = await getDocs(collection(db, "categories"))
-	const categories = querySnapshot.forEach((doc) => {
-		// doc.data() is never undefined for query doc snapshots
-		console.log(doc.id, " => ", doc.data())
+	const categories = []
+    querySnapshot.forEach((doc) => {
+
+		categories.push(doc.data())
 	})
 	return categories
+     
 }
