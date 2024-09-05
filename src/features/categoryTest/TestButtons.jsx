8bdebc3523ad8e9c9486481ba549e0e898985c75
MonaRuz <mona.ruz@seznam.cz>
2024-09-05 19:13:55 +0200

    edited styles on TestButtons component

--- src/features/categoryTest/TestButtons.jsx
+++ src/features/categoryTest/TestButtons.jsx
@@ -1,27 +1,38 @@
 import { useNavigate } from "react-router-dom"
 import Button from "../../components/Button"
-export default function TestButtons({category,isOpenAnswer,setIsOpenAnswer,setWrongAnswerEvent,wrongAnswerEvent}) {
-  const navigate=useNavigate()
+export default function TestButtons({
+	category,
+	isOpenAnswer,
+	setIsOpenAnswer,
+	setWrongAnswerEvent,
+	wrongAnswerEvent,
+}) {
+	const navigate = useNavigate()
 
-  function handleWrongAnswer(){
-	setWrongAnswerEvent(!wrongAnswerEvent)
-	setIsOpenAnswer(false)
-  }
+	function handleWrongAnswer() {
+		setWrongAnswerEvent(!wrongAnswerEvent)
+		setIsOpenAnswer(false)
+	}
 	return (
-		<div className="flex flex-col justify-center items-center gap-1">
-			<div className=" mt-16">
-				{!isOpenAnswer&&<div className="flex flex-col justify-center gap-3">
-					<Button
-						onClick={()=>setIsOpenAnswer(!isOpenAnswer)}
-						style={{
-							backgroundColor: "rgb(254 240 138)",
-							width: "300px",
-							height: "40px",
-							fontFamily: "kanit",
-						}}
-					>Display correct answer</Button>
-				</div>}
-				<div className="flex gap-1 justify-center items-center">
+		<div className='flex flex-col justify-center items-center'>
+			<div className=' mt-16'>
+				{!isOpenAnswer && (
+					<div className='flex flex-col justify-center gap-3'>
+						<Button
+							onClick={() => setIsOpenAnswer(!isOpenAnswer)}
+							style={{
+								backgroundColor: "rgb(254 240 138)",
+								width: "300px",
+								height: "40px",
+								fontFamily: "kanit",
+								marginBottom:"3px"
+							}}
+						>
+							Display correct answer
+						</Button>
+					</div>
+				)}
+				<div className='flex justify-center items-center gap-1'>
 					<Button
 						onClick={handleWrongAnswer}
 						style={{
@@ -30,28 +41,34 @@ export default function TestButtons({category,isOpenAnswer,setIsOpenAnswer,setWr
 							height: "40px",
 							fontFamily: "kanit",
 						}}
-					>Wrong</Button>
+					>
+						Wrong
+					</Button>
 					<Button
-
 						style={{
 							backgroundColor: "#88FFB6",
 							width: "148px",
 							height: "40px",
 							fontFamily: "kanit",
 						}}
-					>Correct</Button>
+					>
+						Correct
+					</Button>
 				</div>
 			</div>
 			<div>
 				<Button
-          onClick={()=>navigate(`/${category}/test/instructions`)}
+					onClick={() => navigate(`/${category}/test/instructions`)}
 					style={{
 						backgroundColor: "rgb(254 240 138)",
 						width: "300px",
 						height: "40px",
 						fontFamily: "kanit",
+						marginTop:"3px"
 					}}
-				>Back</Button>
+				>
+					Back
+				</Button>
 			</div>
 		</div>
 	)
