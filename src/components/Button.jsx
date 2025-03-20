b9e71a994a3951ccb6418b2bc33e93e549b99e58
MonaRuz <mona.ruz@seznam.cz>
2025-03-20 20:05:17 +0100

    Added PropTypes to Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,18 @@
-export default function Button({ style, children, onClick,disabled }) {
-	return (		
-			<button disabled={disabled} className="border-4 border-black hover:border-none" onClick={onClick} style={style}>{children}</button>
-		
+import PropTypes from "prop-types"
+export default function Button({ style, children, onClick }) {
+	return (
+		<button
+			className='border-4 border-black hover:border-none'
+			onClick={onClick}
+			style={style}
+		>
+			{children}
+		</button>
 	)
+}
+
+Button.propTypes = {
+	style: PropTypes.object,
+	children: PropTypes.string,
+	onClick: PropTypes.function,
 }
