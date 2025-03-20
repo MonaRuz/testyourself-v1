a0d1f19ae360e63d5f7e2509a0c29dc669744f0f
MonaRuz <mona.ruz@seznam.cz>
2025-03-20 21:06:17 +0100

    Added propTypes for Spinner component

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import { BarLoader } from "react-spinners";
 
 
@@ -8,4 +9,8 @@ export default function Spinner({children}) {
         <BarLoader color="#fbcfe8" width="300px"/>
     </div>
   )
+}
+
+Spinner.propTypes={
+  children:PropTypes.string
 }
