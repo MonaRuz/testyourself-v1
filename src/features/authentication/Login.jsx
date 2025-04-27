2eca8f306be05abc9f0397ddbc3cdcb354c326d7
MonaRuz <mona.ruz@seznam.cz>
2025-04-27 02:22:48 +0200

    Login/onSubmit do not work

--- src/features/authentication/Login.jsx
+++ src/features/authentication/Login.jsx
@@ -5,57 +5,69 @@ import { useForm } from "react-hook-form"
 
 export default function Login() {
 	const navigate = useNavigate()
-	const {register,handleSubmit,formState,reset}=useForm()
-	function handleLogin(e) {
-		e.preventDefault()
-		navigate("/dashboard")
+	const { register, handleSubmit, formState, reset } = useForm()
+	const {errors}=formState
+	// function handleLogin(e) {
+	// 	e.preventDefault()
+	// 	navigate("/dashboard")
+	// }
+
+	function onSubmit(data) {
+		console.log("test")
 	}
 
 	return (
 		<div className='h-dvh bg-black px-3'>
 			<Logo />
-			<form className='flex flex-col text-center mt-10'>
+			<form
+				onSubmit={handleSubmit(onSubmit)}
+				className='flex flex-col text-center mt-10'
+			>
 				<label className='text-blue-200 my-3'>
 					email<br></br>
 					<input
-					id="email"
 						className='bg-black border border-blue-200 mt-3 h-10 w-72'
+						id='email'
 						type='email'
-						{...register("email",{required:"This field must be filled!"})}
+						{...register("email", { required: "This field must be filled!" })}
 					/>
+					{errors?.email?.message&&<p className="text-red-300 text-sm">{errors.email.message}</p>}
 				</label>
 				<label className='text-blue-200 my-3'>
 					password<br></br>
 					<input
-					id="password"
 						className='bg-black border border-blue-200 mt-3 h-10 w-72'
+						id='password'
 						type='password'
-						{...register("password",{required:"This field must be filled!"})}
+						{...register("password", {
+							required: "This field must be filled!",
+						})}
 					/>
+					{errors?.password?.message&&<p className="text-red-300 text-sm">{errors.password.message}</p>}
 				</label>
 				<label className='hidden'>
 					password<br></br>
 					<input
-					id="age"
 						className='bg-black border border-blue-200 mt-3 h-10 w-72'
+						id='age'
 						type='text'
-						{...register("age",{required:"This field must be filled!"})}
+						{...register("age", { required: "This field must be filled!" })}
 					/>
 				</label>
+				<div className='flex justify-center mt-10'>
+					<Button
+						type='submit'
+						style={{
+							backgroundColor: "#88FFB6",
+							width: "130px",
+							height: "40px",
+							fontFamily: "kanit",
+						}}
+					>
+						Login
+					</Button>
+				</div>
 			</form>
-			<div className='flex justify-center mt-10'>
-				<Button
-					onClick={handleLogin}
-					style={{
-						backgroundColor: "#88FFB6",
-						width: "130px",
-						height: "40px",
-						fontFamily: "kanit",
-					}}
-				>
-					Login
-				</Button>
-			</div>
 			<p
 				onClick={() => navigate("/register")}
 				className='text-blue-300 text-sm text-center mt-2 hover:underline cursor-pointer'
