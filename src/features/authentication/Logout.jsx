b0581b4f0431e8a61d06ba527939accd3c1b81bd
MonaRuz <mona.ruz@seznam.cz>
2025-04-27 21:00:52 +0200

    Spinner in log out

--- src/features/authentication/Logout.jsx
+++ src/features/authentication/Logout.jsx
@@ -3,25 +3,26 @@ import { useNavigate } from "react-router-dom"
 import { getAuth, signOut } from "firebase/auth"
 import toast from "react-hot-toast"
 import { useState } from "react"
+import Spinner from "../../components/Spinner"
 
 export default function Logout() {
-  const[isLoading,setIsLoading]=useState(false)
-  const navigate = useNavigate()
+	const [isLoading, setIsLoading] = useState(false)
+	const navigate = useNavigate()
 	const auth = getAuth()
-	
-  function handleSignOut(){
-    setIsLoading(true)
-    signOut(auth)
-		.then(() => {
-			toast.success("User successfilly logged out")
-		})
-		.catch((error) => {
-			toast.error(error.message)
-		})
-    navigate("/login")
-    setIsLoading(false)
-  }
 
+	function handleSignOut() {
+		setIsLoading(true)
+		signOut(auth)
+			.then(() => {
+				toast.success("User successfilly logged out")
+			})
+			.catch((error) => {
+				toast.error(error.message)
+			})
+		navigate("/login")
+		setIsLoading(false)
+	}
+	if (isLoading) return <Spinner>Sign out...</Spinner>
 	return (
 		<div className='text-red-300 text-3xl hover:text-red-500 hover:cursor-pointer'>
 			<IoMdLogOut onClick={handleSignOut} />
