20f6528b6f5f7493af5a52ed0e28ca8fe56932ac
MonaRuz <mona.ruz@seznam.cz>
2025-04-27 18:12:04 +0200

    Logout finished

--- src/features/authentication/Logout.jsx
+++ src/features/authentication/Logout.jsx
@@ -1,13 +1,30 @@
-import { IoMdLogOut } from "react-icons/io";
-import { useNavigate } from "react-router-dom";
-
+import { IoMdLogOut } from "react-icons/io"
+import { useNavigate } from "react-router-dom"
+import { getAuth, signOut } from "firebase/auth"
+import toast from "react-hot-toast"
+import { useState } from "react"
 
 export default function Logout() {
+  const[isLoading,setIsLoading]=useState(false)
+  const navigate = useNavigate()
+	const auth = getAuth()
+	
+  function handleSignOut(){
+    setIsLoading(true)
+    signOut(auth)
+		.then(() => {
+			toast.success("User successfilly logged out")
+		})
+		.catch((error) => {
+			toast.error(error.message)
+		})
+    navigate("/login")
+    setIsLoading(false)
+  }
 
-  const navigate=useNavigate()
-  return (
-    <div className="text-red-300 text-3xl hover:text-red-500 hover:cursor-pointer">
-        <IoMdLogOut onClick={()=>navigate("/")}/>
-    </div>
-  )
+	return (
+		<div className='text-red-300 text-3xl hover:text-red-500 hover:cursor-pointer'>
+			<IoMdLogOut onClick={handleSignOut} />
+		</div>
+	)
 }
