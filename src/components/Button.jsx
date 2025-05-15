df391d4addb6415acc68bb5cb65503e11a1b551b
MonaRuz <mona.ruz@seznam.cz>
2025-05-15 10:30:35 +0200

    Button/propTypes

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,19 +1,20 @@
 import PropTypes from "prop-types"
-// export default function Button({ style, children, onClick, type, disabled }) {
-// 	return (
-// 		<button
-// 			disabled={disabled}
-// 			className='border-4 border-black hover:border-none'
-// 			onClick={onClick}
-// 			type={`${type}`}
-// 			style={style}
-// 		>
-// 			{children}
-// 		</button>
-// 	)
-// }
 
-export default function Button({ style, children, onClick, type = "button", disabled }) {
+Button.propTypes = {
+	style: PropTypes.object,
+	children: PropTypes.string,
+	onClick: PropTypes.func,
+	type: PropTypes.string,
+	disabled: PropTypes.bool,
+}
+
+export default function Button({
+	style,
+	children,
+	onClick,
+	type = "button",
+	disabled,
+}) {
 	return (
 		<button
 			disabled={disabled}
@@ -25,12 +26,4 @@ export default function Button({ style, children, onClick, type = "button", disa
 			{children}
 		</button>
 	)
-}
-
-Button.propTypes = {
-	style: PropTypes.object,
-	children: PropTypes.string,
-	onClick: PropTypes.func,
-	type: PropTypes.string,
-	disabled:PropTypes.bool
 }
